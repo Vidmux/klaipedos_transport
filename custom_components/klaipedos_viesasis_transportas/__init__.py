8f54a575278585e721dc5f58e697b76b68faef69
Vidmux <vidmantas.paulauskas@gmail.com>
2026-03-05 10:25:18 +0200

    Update __init__.py

--- custom_components/klaipedos_viesasis_transportas/__init__.py
+++ custom_components/klaipedos_viesasis_transportas/__init__.py
@@ -1,1 +1,17 @@
+"""Klaipėdos viešasis transportas integracija."""
+import logging
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
+# TURI SUTAPTI SU MANIFEST
+DOMAIN = "klaipedos_viesasis_transportas"
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Nustatome integraciją."""
+    # Ši eilutė liepia HA užkrauti device_tracker.py
+    await hass.config_entries.async_forward_entry_setups(entry, ["device_tracker"])
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Ištriname integraciją."""
+    return await hass.config_entries.async_unload_platforms(entry, ["device_tracker"])
